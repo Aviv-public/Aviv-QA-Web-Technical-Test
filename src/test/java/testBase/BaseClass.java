421dbe57f2d43afcbc77ab3f739938afe545f34c
Kunal Biraris <birariskunalqa@gmail.com>
2024-02-10 15:03:03 +0100

    removed properties file, added login URL to pom.xml, and used it in BaseClass get method

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -5,31 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-import java.io.FileReader;
-import java.time.Duration;
-import java.util.Properties;
-
 public class BaseClass {
 
     public WebDriver driver;
-    public Properties properties;
 
     @BeforeClass
     public void setup(){
 
-        //loading properties file
-        try {
-            FileReader file = new FileReader(".//src//test//java//resources//config.properties");
-            properties = new Properties();
-            properties.load(file);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
         driver=new ChromeDriver();
         driver.manage().deleteAllCookies();
-
-        driver.get(properties.getProperty("appURL"));
+        driver.get(System.getProperty("login.url"));
         driver.manage().window().maximize();
     }
 
@@ -37,5 +22,4 @@ public class BaseClass {
     public void tearDown(){
         driver.close();
     }
-
 }
