fd71bfbf8a8791b4176e87313977a9a29ffca3ab
Kunal Biraris <birariskunalqa@gmail.com>
2024-02-10 15:23:26 +0100

    Updated BaseClass for cross-browser execution and added WebDriverManager plugin to pom.xml

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,18 +1,31 @@
 package testBase;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 
     public WebDriver driver;
 
     @BeforeClass
-    public void setup(){
+    @Parameters("browser")
+    public void setup(String browser){
 
-        driver=new ChromeDriver();
+        switch (browser.toLowerCase()){
+            case "chrome" : WebDriverManager.chromedriver().setup();
+                            driver = new ChromeDriver();
+                            break;
+            case "edge"   : WebDriverManager.edgedriver().setup();
+                            driver = new EdgeDriver();
+                            break;
+            default       : System.out.println("No matching browser..");
+        }
+
         driver.manage().deleteAllCookies();
         driver.get(System.getProperty("login.url"));
         driver.manage().window().maximize();
