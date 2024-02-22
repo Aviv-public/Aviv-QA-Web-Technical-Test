723efdedc24417d72ad3b3ec362e63a87a7e1a1e
Ayesha Raheed <ayeshanadeem291@gmail.com>
2024-02-22 15:10:48 +0100

    login with an existing user and perform checkout

--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -1,3 +1,5 @@
+import Register from "../pages/registerPage";
+const register = new Register();
 class Login {
     loginLink = "a.ico-login";
     email = "input[id='Email']";
@@ -10,5 +12,27 @@ class Login {
       cy.get(this.password).type(password);
       cy.get(this.loginButton).click();
     }
+    Auth(email, password) {
+        // Check for login error
+        this.login(email, password);
+        cy.url().then((currentUrl) => {
+          if (currentUrl === "https://demo.nopcommerce.com/login?returnurl=%2F") {
+            register.registerNewUser({
+              firstName: "Ayesha",
+              lastName: "Raheed",
+              email: "ayesha@gmail.com",
+              password: "ayeshamysis2",
+              confirmPassword: "ayeshamysis2",
+            });
+    
+            // Optionally, redirect to the home page after registration
+            register.redirectToHomePage();
+            this.login(email, password);
+          } else {
+            // No error message, continue with your logic
+            cy.visit("https://demo.nopcommerce.com/");
+          }
+        });
+      }
   }
   export default Login;
